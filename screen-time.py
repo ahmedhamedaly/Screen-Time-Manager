b5fa999efbb079fa40fa9dc2f308a9e6ea818c11
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-04 20:34:20 +0000

    integration for windows, mac and linux

--- screen-time.py
+++ screen-time.py
@@ -1,11 +1,34 @@
-import win32gui as win
+import sys
+
+if sys.platform in ['Windows', 'win32', 'cygwin']:
+    import win32gui
+    windows = True
+elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
+    from AppKit import NSWorkspace
+    mac = True
+elif sys.platform in ['linux', 'linux2']:
+    import linux
+    linux = True
+else:
+    print("No Support for your current OS")
 
 prevWindow = None
 
-while True:
-    currWindow = win.GetWindowText(win.GetForegroundWindow())
-    currWindow = currWindow.split('-')[-1].lstrip(' ')
+def getCurrentWindow():
+    if windows:
+        c = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+        return c.split('-')[-1].lstrip(' ')
+    elif mac:
+        return (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName'])
+    elif linux:
+        return linux.get_active_window_x()
+    else:
+        return None
 
-    if prevWindow != currWindow and currWindow != " ":
+while True:
+    
+    currWindow = getCurrentWindow()
+
+    if prevWindow != currWindow:
         prevWindow = currWindow        
         print(currWindow)
